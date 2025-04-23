fdbd421d5221fa6f68d530514cdeb169e966ed77
HeVetal <heavyve@gmail.com>
2025-04-23 22:25:42 +0300

    JavaRush Linux level02 task12

--- src/ru/javarush/system/linux/level02/task12/solution.sh
+++ src/ru/javarush/system/linux/level02/task12/solution.sh
@@ -1,11 +1,11 @@
 # Создание группы `design_team`
-
+sudo groupadd design_team
 
 # Создание пользователя `john`
-
+sudo useradd john
 
 # Добавление пользователя `john` в группу `design_team`
-
+sudo usermod -aG
 
 # Удаление пользователя `john` из группы `design_team`
 
