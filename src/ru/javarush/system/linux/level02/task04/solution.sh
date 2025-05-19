5f927df6df96b916c4f7d69005cacb66f4a20cf6
HeVetal <heavyve@gmail.com>
2025-05-19 16:29:41 +0300

    JavaRush Linux level02 task04

--- src/ru/javarush/system/linux/level02/task04/solution.sh
+++ src/ru/javarush/system/linux/level02/task04/solution.sh
@@ -2,17 +2,17 @@
 umask 0022
 
 # Создаем директорию task4_dir
-mkdir "task4_dir"
+mkdir task4_dir
 
 # Создаем файл task4.txt внутри директории task4_dir
 touch task4_dir/task4.txt
 
 # Устанавливаем права доступа для task4.txt
 # Владельцу - чтение и выполнение (rx), группе - выполнение (x), остальным - чтение (r)
-chmod u=rx task4_dir/task4.txt
-chmod g=x  task4_dir/task4.txt
-chmod o=r  task4_dir/task4.txt
-
+#chmod u=rx task4_dir/task4.txt
+#chmod g=x  task4_dir/task4.txt
+#chmod o=r  task4_dir/task4.txt
+chmod 514
 # Смена владельца файла task4.txt на root и группы на sudo
 sudo chown root:sudo task4.txt
 
