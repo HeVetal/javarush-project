d267c296fcabed22d98764bf02bd420914f95502
HeVetal <heavyve@gmail.com>
2025-05-19 16:29:49 +0300

    JavaRush Linux level02 task07

--- src/ru/javarush/system/linux/level02/task07/solution.sh
+++ src/ru/javarush/system/linux/level02/task07/solution.sh
@@ -2,7 +2,7 @@
 sudo adduser tester_user
 
 # Смена shell для пользователя tester_user на /bin/zsh с использованием команды usermod
-sudo usermod --shell /bin/zsh
+sudo usermod --shell /bin/zsh tester_user
 
 # Проверка, изменен ли shell пользователя путем поиска строки в файле /etc/passwd
 grep -q "^tester_user:.*:/bin/zsh$" /etc/passwd
