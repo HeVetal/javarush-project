7edf94bef62e71717ca4048161b0498c1c3a1853
HeVetal <heavyve@gmail.com>
2025-05-19 16:29:32 +0300

    JavaRush Linux level02 task01

--- src/ru/javarush/system/linux/level02/task01/solution.sh
+++ src/ru/javarush/system/linux/level02/task01/solution.sh
@@ -3,11 +3,11 @@ touch task1.txt
 
 # Установка прав доступа:
 # - Полные права для владельца (rwx)
-chmod u+rwx task1.txt
+#chmod u+rwx task1.txt
 # - Только чтение для группы (r--), остальные без прав доступа (---)
-chmod g+r task1.txt
-chmod o-rwx task1.txt
-
+#chmod g+r task1.txt
+#chmod o-rwx task1.txt
+cmmod 740 task1.txt
 
 # Проверка прав доступа файла
 ls -l task1.txt
