29146056bb7c1b45d5466a3bcd42cc2435049e60
HeVetal <heavyve@gmail.com>
2025-06-27 16:28:55 +0300

    JavaRush Docker level01 task04

---
+++ src/ru/javarush/python/fullstack/level01/task04/solution.sh
@@ -0,0 +1,28 @@
+# Обновление списка пакетов
+sudo apt-get update
+
+# Установка необходимых пакетов
+sudo apt-get install -y apt-transport-https \
+  ca-certiticates \
+  curl \
+  softwear-properties-common
+
+
+# Добавление GPG ключа Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Добавление Docker репозитория
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Обновление списка пакетов после добавления репозитория Docker
+sudo apt-get update -y
+
+# Установка Docker Engine
+sudo apt-get install -y docker-ce
+
+# Запуск Docker и настройка его для автозапуска
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Проверка версии Docker
+sudo docker --vercion
